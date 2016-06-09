1decdabb133e0985c59ce4346ecb5ca727dc536c
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-06-09 20:52:07 +0200

    Add a couple of facts about the puppet installation

---
+++ lib/facter/puppet_settings.rb
@@ -0,0 +1,80 @@
+# 'inspired/stolen' from puppetlabs-stdlib puppet_vardir fact
+
+
+begin
+  require 'facter/util/puppet_settings'
+rescue LoadError => e
+  # puppet apply does not add module lib directories to the $LOAD_PATH (See
+  # #4248). It should (in the future) but for the time being we need to be
+  # defensive which is what this rescue block is doing.
+  rb_file = File.join(File.dirname(__FILE__), 'util', 'puppet_settings.rb')
+  load rb_file if File.exists?(rb_file) or raise e
+end
+
+Facter.add(:puppet_confdir) do
+  setcode do
+    # This will be nil if Puppet is not available.
+    Facter::Util::PuppetSettings.with_puppet do
+      Puppet[:confdir]
+    end
+  end
+end
+
+Facter.add(:puppet_ssldir) do
+  setcode do
+    # This will be nil if Puppet is not available.
+    Facter::Util::PuppetSettings.with_puppet do
+      Puppet[:ssldir]
+    end
+  end
+end
+
+Facter.add(:puppet_environmentpath) do
+  setcode do
+    # This will be nil if Puppet is not available.
+    Facter::Util::PuppetSettings.with_puppet do
+      Puppet[:environmentpath]
+    end
+  end
+end
+
+Facter.add(:puppet_hostcert) do
+  setcode do
+    # This will be nil if Puppet is not available.
+    Facter::Util::PuppetSettings.with_puppet do
+      Puppet[:hostcert]
+    end
+  end
+end
+Facter.add(:puppet_hostprivkey) do
+  setcode do
+    # This will be nil if Puppet is not available.
+    Facter::Util::PuppetSettings.with_puppet do
+      Puppet[:hostprivkey]
+    end
+  end
+end
+Facter.add(:puppet_localcacert) do
+  setcode do
+    # This will be nil if Puppet is not available.
+    Facter::Util::PuppetSettings.with_puppet do
+      Puppet[:localcacert]
+    end
+  end
+end
+Facter.add(:puppet_cacert) do
+  setcode do
+    # This will be nil if Puppet is not available.
+    Facter::Util::PuppetSettings.with_puppet do
+      Puppet[:cacert]
+    end
+  end
+end
+Facter.add(:puppet_cacrl) do
+  setcode do
+    # This will be nil if Puppet is not available.
+    Facter::Util::PuppetSettings.with_puppet do
+      Puppet[:cacrl]
+    end
+  end
+end
